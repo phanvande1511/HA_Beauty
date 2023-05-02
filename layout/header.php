1a8b304b1801ff1e4d1c1d48b3561517daa4709d
Phan Van De <phanvande2003@gmail.com>
2023-05-02 10:25:42 +0700

    first commit

---
+++ layout/header.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="layout/layout.css">
+</head>
+<body>
+	<!-- Header -->
+	<nav id="header_navbar">
+		<!-- Grid -->
+		<div class="grid">
+			<!-- Header container -->
+			<div class="header_container">
+				<!-- Header logo -->
+				<div class="header_logo">
+					<img class="header_logo_header" src="./images/logo-home.png">
+				</div>
+				<!-- Header search -->
+				<div class="header_search">
+
+				</div>
+				<!-- Header Account -->
+				<div class="header_account">
+
+				</div>
+				<!-- Header cart-->
+				<div class="header_cart">
+
+				</div>
+			</div>
+		</div>
+	</nav>
+</body>
+</html>
